e13f400db14733a120840a492cd74c263c0c973b
Zbynkus9 <kamil.pojedynek@gmail.com>
2024-11-23 13:25:25 +0100

    poprawiona zamiana postaci

--- Assets/Scripts/PlayerSwitch.cs
+++ Assets/Scripts/PlayerSwitch.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
+using UnityEngine.TextCore.Text;
 
 public class CharacterManager : MonoBehaviour
 {
     public GameObject[] characters; // Assign characters in the inspector
     private int currentCharacterIndex = 0;
-    int curChar;
+    //int curChar;
+    int perwChar;
+    int nextChar;
     public CinemachineVirtualCamera cam; // Assign virtual camera in the inspector
 
     void Start()
@@ -21,26 +24,61 @@ public class CharacterManager : MonoBehaviour
         // Example input for switching: Press "Tab" to switch characters
         if (Keyboard.current.tabKey.wasPressedThisFrame)
         {
-            curChar = currentCharacterIndex;
+            Debug.Log("Tab key was pressed");
             // Check if the next character is too far away
-            if (Vector3.Distance(characters[currentCharacterIndex].transform.position, characters[(currentCharacterIndex + 1) % characters.Length].transform.position) > 5f)
+            foreach (GameObject character in characters)
             {
-                Debug.Log("Too far away from the next character");
-                currentCharacterIndex = curChar;
-                return;
+                if (character == characters[currentCharacterIndex])
+                {
+                    continue;
+                }
+                if (character == characters[perwChar])
+                {
+                    continue;
+                }
+                Debug.Log(Vector3.Distance(characters[currentCharacterIndex].transform.position, character.transform.position));
+                if (Vector3.Distance(characters[currentCharacterIndex].transform.position, character.transform.position) < 5f)
+                {
+                    Debug.Log("Close enough");
+                    nextChar = System.Array.IndexOf(characters, character);
+                    break;
+                }
             }
+            //if (Vector3.Distance(characters[currentCharacterIndex].transform.position, characters[(currentCharacterIndex + 1) % characters.Length].transform.position) > 5f)
+            //{
+            //    Debug.Log("Too far away from the next character");
+            //    currentCharacterIndex = curChar;
+            //    return;
+            //}
+            Debug.Log("Switching to next character");
+            //UpdateControl(currentCharacterIndex);
             SwitchToNextCharacter();
         }
     }
 
     public void SwitchToNextCharacter()
     {
-        Debug.Log("Switching to next character");
+        Debug.Log("Switching to next character is in progress");
         // Disable current character
         SetCharacterControl(currentCharacterIndex, false);
 
         // Move to the next character
-        currentCharacterIndex = (currentCharacterIndex + 1) % characters.Length;
+        //currentCharacterIndex = (currentCharacterIndex + 1) % characters.Length;
+        if (nextChar == currentCharacterIndex)
+        {
+            if (Vector3.Distance(characters[currentCharacterIndex].transform.position, characters[perwChar].transform.position) < 5f)
+            {
+                Debug.Log("Close enough");
+                currentCharacterIndex = perwChar;
+                nextChar = perwChar;
+            }
+            
+        }
+        else
+        {
+            perwChar = currentCharacterIndex;
+            currentCharacterIndex = nextChar;
+        }
 
         // Enable the new character
         SetCharacterControl(currentCharacterIndex, true);
@@ -62,7 +100,11 @@ public class CharacterManager : MonoBehaviour
         {
             playerInput.enabled = isActive;
         }
-        cam.LookAt = characters[characterIndex].transform.GetChild(0);
-        cam.Follow = characters[characterIndex].transform.GetChild(0);
+        if (isActive)
+        {
+            cam.LookAt = characters[characterIndex].transform.GetChild(0);
+            cam.Follow = characters[characterIndex].transform.GetChild(0);
+        }
+        
     }
 }
