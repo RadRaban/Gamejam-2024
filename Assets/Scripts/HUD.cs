4750456dcadecaefa77c9ff1aa250c768e06d31a
Zbynkus9 <kamil.pojedynek@gmail.com>
2024-11-24 15:06:15 +0100

    hud i poprawione punkty

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,21 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.SceneManagement;
 using TMPro;
 using TMPro.Examples;
 
 public class HUDUI : MonoBehaviour
 {
-    [SerializeField] public GameObject HUD;
-
     public TextMeshProUGUI counterText;
 
-    public int s = scoring.score;
-    //public int collected = s / 50;
-    
+    public GameObject[] fiolki;
+    public int ile = 0;
+
+    public int s;
+
+    private void Start()
+    {
+        fiolki = GameObject.FindGameObjectsWithTag("Fiolka");
+        ile = fiolki.Length;
+        s = scoring.score;
+        counterText.text = (s/50) + " / " + ile;
+    }
 
     void FixedUpdate()
     {
-        
+        s = scoring.score;
+        counterText.text = (s / 50) + " / " + ile;
     }
 
 
